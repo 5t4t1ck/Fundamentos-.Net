367729957dbfe63ed538c1ea94cc87f40fb7907a
rcarmas <rcarmas@espe.edu.ec>
2023-06-19 19:41:44 -0500

    Update Ejer clase

--- Colaboraciones/Rodney-Armas/MiProyecto/Program.cs
+++ Colaboraciones/Rodney-Armas/MiProyecto/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Humanizer;
+
+Console.WriteLine("Por favor ingrese un nombre");
+var apellido = Console.ReadLine();
+Console.WriteLine("Por favor ingrese su signo");
+var signo = Console.ReadLine();
+Console.WriteLine("Por favor ingrese su dia de nacimiento:");
+var diaN = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Hola, Mi nombre es {apellido}, soy {signo} y cumplo el {diaN.ToWords(new System.Globalization.CultureInfo("es"))} mayo.");
